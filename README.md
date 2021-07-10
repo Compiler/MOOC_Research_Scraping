275c0fef362db476802a42f4aabeb9145ed976e5
compiler <lukerochedev@gmail.com>
2021-07-10 08:24:10 -0400

    file structuring

--- README.md
+++ README.md
@@ -7,6 +7,6 @@
 - Run the line in the requirements.txt on cmd within root of clone
 
 - run either of the following:
-> python core.py
+> python src/core.py
 
-> python core_analytics.py
+> python src/core_analytics.py
--- src/core.py
+++ src/core.py
@@ -25,24 +25,5 @@ def get_channel_metrics(youtube_instance, channel_id):
 
     print(res)
 
-
-
-
-def execute_api_request(client_library_function, **kwargs):
-  response = client_library_function(
-    **kwargs
-  ).execute()
-
-  print(response)
-
 get_channel_metrics(youtube, RowanRobotics_channel_id)
 
-execute_api_request(
-      youtube.reports().query,
-      ids='channel==MINE',
-      startDate='2017-01-01',
-      endDate='2017-12-31',
-      metrics='estimatedMinutesWatched,views,likes,subscribersGained',
-      dimensions='day',
-      sort='day'
-  )
